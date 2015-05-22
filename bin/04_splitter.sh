15d5addb0d28e25573e138c9cb8da2f48eb8eebc
Martin Andres Gomez Gimenez <mggimenez@ingeniovirtual.com.ar>
2015-05-22 11:28:14 -0300

    Se agrega la opción --wanted-admin-level=4 para asegurase de que los pequeños límites administrativos se mantienen completos.

--- bin/04_splitter.sh
+++ bin/04_splitter.sh
@@ -17,7 +17,8 @@ G='\E[1;32;40m'
 Y='\E[1;33;40m'
 W='\E[0;38;40m'
 
-COMMON_OPTIONS="--keep-complete=true --max-areas=512 --max-nodes=600000 --no-trim"
+COMMON_OPTIONS="--keep-complete=true --wanted-admin-level=4 --max-areas=512"
+COMMON_OPTIONS="${COMMON_OPTIONS} --max-nodes=600000 --no-trim"
 COMMON_OPTIONS="${COMMON_OPTIONS} --overlap=0 --output=xml --resolution=12"
 
 rm -f template.args
