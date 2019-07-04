ed430a7f4cf351ef708ccb4cc0d6f5e3cba75fd1
Martin Andres Gomez Gimenez <mggimenez@ingeniovirtual.com.ar>
2019-07-04 20:59:57 -0300

    Cambios para descargar versiones pre-compiladas de mkgmap y splitter.

--- bin/build_mkgmap.sh
+++ bin/build_mkgmap.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 #
-# build_mkgmap.sh: script descargar y compilar mkgmap.jar
+# build_mkgmap.sh: script descargar e instalar:
+# * mkgmap
+# * splitter
+# * osmconvert
+# * osmfilter
+# * pbftoosm
 #
-# (C) 2012 - 2015 Martin Andres Gomez Gimenez <mggimenez@ingeniovirtual.com.ar>
+# (C) 2012 - 2019 Martin Andres Gomez Gimenez <mggimenez@ingeniovirtual.com.ar>
 # Distributed under the terms of the GNU General Public License v3
 #
 
-# Requiere los siguientes paquetes:
-# gcc: http://gcc.gnu.org/
-
 
 WORKDIR=`pwd`
+MKGMAP_DOWNLOAD_URL="http://www.mkgmap.org.uk/download"
+MKGMAP_VERSION="4262"
+SPLITTER_VERSION="592"
 
 
 OSMCONVERT_URL="http://m.m.i24.cc/osmconvert.c"
@@ -23,24 +28,20 @@ PBFTOOSM="${WORKDIR}/bin/pbftoosm"
 
 
 
-rm -rf ${WORKDIR}/mkgmap
-rm -rf ${WORKDIR}/splitter
+rm -rf ${WORKDIR}/mkgmap*
+rm -rf ${WORKDIR}/splitter*
 
+wget -c ${MKGMAP_DOWNLOAD_URL}/mkgmap-r${MKGMAP_VERSION}.zip
+wget -c ${MKGMAP_DOWNLOAD_URL}/splitter-r${SPLITTER_VERSION}.zip
 
-svn export http://svn.mkgmap.org.uk/mkgmap/trunk mkgmap
+unzip mkgmap-r${MKGMAP_VERSION}.zip
+unzip splitter-r${SPLITTER_VERSION}.zip
 
-# Se compila mkgmap
-cd ${WORKDIR}/mkgmap
-ant
+rm -f mkgmap-r${MKGMAP_VERSION}.zip
+rm -f splitter-r${SPLITTER_VERSION}.zip
 
-
-cd ${WORKDIR}
-svn export http://svn.mkgmap.org.uk/splitter/trunk splitter
-
-# Se compila splitter
-cd ${WORKDIR}/splitter
-ant
-
+ln --symbolic mkgmap-r${MKGMAP_VERSION} mkgmap
+ln --symbolic splitter-r${SPLITTER_VERSION} splitter
 
 wget -O - ${OSMCONVERT_URL} | cc -x c - -lz -O3 -o  ${OSMCONVERT} &>/dev/null
 wget -O -  ${OSMFILTER_URL} | cc -x c - -O3 -o ${OSMFILTER} &>/dev/null
