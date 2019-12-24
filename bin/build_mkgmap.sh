aa66ecdb7f2414587660019c540f4eaf64f6371a
Martin Andres Gomez Gimenez <mggimenez@ingeniovirtual.com.ar>
2019-12-23 23:32:19 -0300

    Cambios para un mejor manejo de versiones. Se utiliza mkgmap-default-typ-r4268 como versión por defecto.

--- bin/build_mkgmap.sh
+++ bin/build_mkgmap.sh
@@ -14,8 +14,8 @@
 
 WORKDIR=`pwd`
 MKGMAP_DOWNLOAD_URL="http://www.mkgmap.org.uk/download"
-MKGMAP_VERSION="4262"
-SPLITTER_VERSION="595"
+MKGMAP_VERSION="default-typ-r4268"
+SPLITTER_VERSION="r595"
 
 
 OSMCONVERT_URL="http://m.m.i24.cc/osmconvert.c"
@@ -31,17 +31,17 @@ PBFTOOSM="${WORKDIR}/bin/pbftoosm"
 rm -rf ${WORKDIR}/mkgmap*
 rm -rf ${WORKDIR}/splitter*
 
-wget -c ${MKGMAP_DOWNLOAD_URL}/mkgmap-r${MKGMAP_VERSION}.zip
-wget -c ${MKGMAP_DOWNLOAD_URL}/splitter-r${SPLITTER_VERSION}.zip
+wget -c ${MKGMAP_DOWNLOAD_URL}/mkgmap-${MKGMAP_VERSION}.zip
+wget -c ${MKGMAP_DOWNLOAD_URL}/splitter-${SPLITTER_VERSION}.zip
 
-unzip mkgmap-r${MKGMAP_VERSION}.zip
-unzip splitter-r${SPLITTER_VERSION}.zip
+unzip mkgmap-${MKGMAP_VERSION}.zip
+unzip splitter-${SPLITTER_VERSION}.zip
 
-rm -f mkgmap-r${MKGMAP_VERSION}.zip
-rm -f splitter-r${SPLITTER_VERSION}.zip
+rm -f mkgmap-${MKGMAP_VERSION}.zip
+rm -f splitter-${SPLITTER_VERSION}.zip
 
-ln --symbolic mkgmap-r${MKGMAP_VERSION} mkgmap
-ln --symbolic splitter-r${SPLITTER_VERSION} splitter
+ln --symbolic mkgmap-${MKGMAP_VERSION} mkgmap
+ln --symbolic splitter-${SPLITTER_VERSION} splitter
 
 wget -O - ${OSMCONVERT_URL} | cc -x c - -lz -O3 -o  ${OSMCONVERT} &>/dev/null
 wget -O -  ${OSMFILTER_URL} | cc -x c - -O3 -o ${OSMFILTER} &>/dev/null
